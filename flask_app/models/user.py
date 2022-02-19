284456c392c4347b200da90fd7b0578823f0dc53
Emily Nesbitt <emily3nesbitt@gmail.com>
2022-02-19 17:56:31 -0500

    trying to add validation

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,4 +1,9 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+from flask_bcrypt import Bcrypt
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
     db_name='users'
@@ -6,8 +11,8 @@ class User:
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-       # self.username = data['username']
-       # self.password = data['password']
+        self.username = data['username']
+        self.password = data['password']
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
@@ -44,6 +49,24 @@ class User:
         query  = "SELECT * FROM users WHERE id = %(id)s";
         result = connectToMySQL('users').query_db(query,data)
         return cls(result[0])
+
+    @staticmethod
+    def validate_user(data):
+        is_valid = True # we assume this is true
+        if len(data['first_name']) < 2:
+            flash('Oops! Name must be at least 2 characters.', 'first_name')
+            is_valid = False
+        if len(data['last_name']) < 2:
+            flash('Oops! Name must be at least 2 characters.', 'last_name')
+            is_valid = False
+        if len(data['password']) < 8:
+            flash("Oops! Password must have at least 8 characters.", 'password')
+            is_valid = False
+        # test whether a field matches the pattern
+        if not EMAIL_REGEX.match(data['email']): 
+            flash("Invalid email address!", 'email')
+            is_valid = False
+        return is_valid
 
 
 
